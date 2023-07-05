9c1708c80442f9f00c0d554dab1c91ad371ade4d
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-07-05 18:54:08 -0300

    Improve UI & add icons

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -6,7 +6,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     return (
         <section className="w-full bg-white">
             <div className="mx-auto max-w-screen-xl px-4">
-                <div className="py-12">{children}</div>
+                <div className="py-6">{children}</div>
             </div>
         </section>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import useImageStore from '@/app/state/imageStore';
+import { FaTimes as Close, FaTrash as Trash, FaEdit as Edit, FaSave as Save } from 'react-icons/fa';
+import { FaMagnifyingGlass as Zoom, FaArrowsRotate as Rotate, FaCheck as Check } from 'react-icons/fa6';
 import React, { useCallback, useState } from 'react';
 import Image from 'next/image';
 import { Range, Slider, Thumb, Track } from '@radix-ui/react-slider';
@@ -54,31 +56,26 @@ const App = () => {
         <>
             {imageSrc && showEdit ? (
                 <>
-                    <div id="cropper-container" className="absolute inset-0 bottom-[136px]">
+                    <div id="cropper-container" className="absolute inset-0 bottom-[167.2px]">
                         <Cropper
                             image={imageSrc}
-                            crop={crop}
                             rotation={rotation}
+                            crop={crop}
                             zoom={zoom}
                             aspect={9 / 16}
                             onCropChange={setCrop}
-                            onRotationChange={setRotation}
                             onCropComplete={onCropComplete}
-                            onZoomChange={setZoom}
                             objectFit="vertical-cover"
                         />
                     </div>
-                    <div
-                        id="controls"
-                        className="absolute bottom-0 flex w-full flex-col items-center gap-4 bg-white p-4"
-                    >
+                    <div id="controls" className="absolute bottom-0 flex w-full flex-col gap-6 bg-white px-4 py-6">
                         <div id="zoom" className="flex items-center gap-4">
-                            <p className="rounded-md bg-gray-800 px-3.5 py-1 text-xs font-medium text-gray-100">Zoom</p>
+                            <Zoom className="mx-2 h-6 w-auto" />
                             <Slider
-                                className="group relative flex h-5 w-[200px] touch-none select-none items-center"
+                                className="group relative flex h-5 w-full touch-none select-none items-center"
                                 defaultValue={[0]}
                                 min={1}
-                                max={3}
+                                max={2}
                                 step={0.1}
                                 value={[zoom]}
                                 onValueChange={(e) => setZoom(e[0])}
@@ -91,17 +88,16 @@ const App = () => {
                                     aria-label="Zoom"
                                 />
                             </Slider>
+                            <input type="number" value={zoom} className="text-md m-0 w-10 p-0 text-center" />
                         </div>
                         <div id="rotation" className="flex items-center gap-4">
-                            <p className="rounded-md bg-gray-800 px-3.5 py-1 text-xs font-medium text-gray-100">
-                                Rotation
-                            </p>
+                            <Rotate className="mx-2 h-6 w-auto" />
                             <Slider
-                                className="group relative flex h-5 w-[200px] touch-none select-none items-center"
+                                className="group relative flex h-5 w-full touch-none select-none items-center"
                                 defaultValue={[0]}
                                 min={0}
                                 max={360}
-                                step={10}
+                                step={15}
                                 value={[rotation]}
                                 onValueChange={(e) => setRotation(e[0])}
                             >
@@ -113,21 +109,24 @@ const App = () => {
                                     aria-label="Rotation"
                                 />
                             </Slider>
+                            <input type="number" value={rotation} className="text-md m-0 w-10 p-0 text-center" />
                         </div>
-                        <div className="flex gap-3">
+                        <div className="flex justify-center gap-3">
                             <button
                                 onClick={() => {
                                     setShowEdit(false);
                                 }}
-                                className="flex items-center justify-between rounded-md border-2 border-gray-600 px-6 py-1 text-xs font-semibold text-gray-800 outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
+                                className="flex items-center justify-between gap-2 rounded-full border-2 border-gray-600 px-6 py-1.5 text-xs font-semibold text-gray-800 outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
                             >
-                                <span>Cancel</span>
+                                <Close />
+                                <span className="pointer-events-none">Cancelar</span>
                             </button>
                             <button
                                 onClick={showCroppedImage}
-                                className="flex items-center justify-between rounded-md border-2 border-transparent bg-gray-800 px-6 py-1 text-xs font-medium text-white outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
+                                className="flex items-center justify-between gap-2 rounded-full border-2 border-transparent bg-gray-800 px-6 py-1.5 text-xs font-medium text-white outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
                             >
-                                <span>Submit</span>
+                                <Check />
+                                <span className="pointer-events-none">Guardar</span>
                             </button>
                         </div>
                     </div>
@@ -141,27 +140,40 @@ const App = () => {
                                 width={504}
                                 src="/phone-bg.png"
                                 alt="Cropped"
-                                className="pointer-events-none absolute inset-0 top-12 mx-auto aspect-[9/16] h-auto max-w-[288px] rounded-lg"
+                                className="pointer-events-none absolute inset-0 top-6 mx-auto aspect-[9/16]  max-w-[288px] rounded-lg"
                             />
                             <Image
                                 height={896}
                                 width={504}
                                 src={croppedImage}
                                 alt="Cropped"
-                                className="mx-auto aspect-[9/16] h-auto max-w-[288px] rounded-lg"
+                                className="mx-auto aspect-[9/16]  max-w-[288px] rounded-lg"
                             />
-                            <div className="flex justify-center gap-3 pt-4">
+                            <div className="flex flex-col justify-center gap-4 pt-4">
                                 <button
-                                    onClick={() => setCroppedImage(null)}
-                                    className="flex items-center justify-between rounded-md border-2 border-gray-600 px-6 py-1 text-xs font-semibold text-gray-800 outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
+                                    onClick={() => {
+                                        setCroppedImage(null);
+                                        setImageSrc(null);
+                                    }}
+                                    className="ring-red/20 mx-auto flex w-full max-w-[288px] items-center justify-center gap-2 rounded-full border-2 border-red-600 px-6 py-2 text-sm font-semibold text-red-800 outline-none ring-red-800/20 transition-all duration-300 hover:ring-4 active:ring-8"
                                 >
-                                    <span>Delete</span>
+                                    <Trash />
+                                    <span className="pointer-events-none">Eliminar</span>
                                 </button>
+
                                 <button
                                     onClick={() => setShowEdit(true)}
-                                    className="flex items-center justify-between rounded-md border-2 border-transparent bg-gray-800 px-6 py-1 text-xs font-medium text-white outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
+                                    className="mx-auto flex w-full max-w-[288px] items-center justify-center gap-2 rounded-full border-2 border-gray-600 px-6 py-2 text-sm font-semibold text-gray-800 outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
                                 >
-                                    <span>Edit</span>
+                                    <Edit />
+                                    <span className="pointer-events-none">Editar</span>
+                                </button>
+                                <button
+                                    // onClick={}
+                                    className="mx-auto flex w-full max-w-[288px] items-center justify-center gap-2 rounded-full border-2 border-transparent bg-gray-800 px-6 py-2 text-sm font-medium text-white outline-none ring-black/20 transition-all duration-300 hover:ring-4 active:ring-8"
+                                >
+                                    <Save />
+                                    <span className="pointer-events-none">Guardar</span>
                                 </button>
                             </div>
                         </>
@@ -172,7 +184,7 @@ const App = () => {
                                 width={504}
                                 src="/phone-bg.png"
                                 alt="Cropped"
-                                className="pointer-events-none absolute inset-0 top-12 mx-auto aspect-[9/16] h-auto max-w-[288px] rounded-lg"
+                                className="pointer-events-none absolute inset-0 top-6 mx-auto aspect-[9/16]  max-w-[288px] rounded-lg"
                             />
                             <Dropzone onFileChange={onFileChange} />
                         </>
